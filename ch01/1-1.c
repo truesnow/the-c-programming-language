894de33f487c0999796b675ffa9b481a1e9ba903
chenxu <chris.chen@yingzt.com>
2020-04-19 15:11:56 +0800

    Add 1-2 answer

--- ch01/1-1.c
+++ ch01/1-1.c
@@ -4,7 +4,7 @@
  * 练习 1-1
  * 在你自己的系统中运行「hello, world」程序。再有意去掉程序中的部分内容，看看会得到什么出错信息。
  */
-main()
+int main()
 {
     printf("hello, world");
 }
---
+++ ch01/1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * 练习 1-2
+ * 做个实验，当 printf 函数的函数字符串中包含 `\c` 时，观察一下会出现什么情况
+ */
+int main() {
+    printf("hello, world\c");
+}
